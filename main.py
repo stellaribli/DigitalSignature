a4a43fec5d9aad29e2c505ff28d8bdc79fae6a75
Stella Ribli <18219027@std.stei.itb.ac.id>
2022-04-14 18:31:07 +0700

    checkpoint 2: insert satu done

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import hashlib
 import pytesseract
 import cv2
 
+namaFile = ''
 #FUNGSI KEY
 def gcd(x,y):
     if x > y:
@@ -122,6 +123,7 @@ pubKeyFile = open('PublicKey.pub','r')
 pubKey = int(pubKeyFile.read())
 
 filename = 'test.txt'
+
 #InsertButton
 def insertButtonSameFile(filename,p,q,privKey):
     signature = encSignature(hashSHA(filename),p*q,privKey)
@@ -137,5 +139,108 @@ def verifyButtonSameFile(filename,p,q,pubKey):
     else:
         return False
 
-insertButtonSameFile('test.txt',p,q,privKey)
-print(verifyButtonSameFile('test.txt',p,q,pubKey))
+# insertButtonSameFile('test.txt',p,q,privKey)
+# print(verifyButtonSameFile('test.txt',p,q,pubKey))
+
+class Landing(QDialog):
+    def __init__(self):
+        super(Landing, self).__init__()
+        loadUi("landingpage.ui", self)
+        self.disatukan.clicked.connect(self.goToSatukan)
+    def goToSatukan(self):
+        widget.setCurrentIndex(1)
+
+class Satu(QDialog):
+    def __init__(self):
+        super(Satu, self).__init__()
+        loadUi("disatukan.ui", self)
+        self.backButton.clicked.connect(self.goBack)
+        self.verifikasi.clicked.connect(self.goToVerif)
+        self.insert.clicked.connect(self.goToInsert)
+    def goBack(self):
+        widget.setCurrentIndex(0)    
+    def goToVerif(self):
+        widget.setCurrentIndex(2)    
+    def goToInsert(self):
+        widget.setCurrentIndex(3)    
+
+class VerifSatu(QDialog):
+    def __init__(self):
+        super(VerifSatu, self).__init__()
+        loadUi("verifSigSatu.ui", self)
+        self.backButton.clicked.connect(self.goBack)
+    def goBack(self):
+        widget.setCurrentIndex(1) 
+
+class InsertSatu(QDialog):
+    def __init__(self):
+        super(InsertSatu, self).__init__()
+        loadUi("insertSigSatu.ui", self)
+        self.backButton.clicked.connect(self.goBack)
+        self.genButton.clicked.connect(self.genKey)
+        self.uploadButton.clicked.connect(self.upload)
+        self.uploadedFile = None 
+        self.namaFile = ''
+        self.insertSig.clicked.connect(self.result)
+
+    def goBack(self):
+        widget.setCurrentIndex(1)  
+    def genKey(self):
+        nilaiP = self.nilaip.text()
+        nilaiQ = self.nilaiq.text()
+        if nilaiP == '' or nilaiQ == '':
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error")
+            msg.setInformativeText('P dan Q belum diinput!')
+            msg.exec_()
+        else:
+            generateKey(int(nilaiP),int(nilaiQ))
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("Berhasil")
+            msg.setInformativeText('Kunci Publik dan Private Berhasil Dibuat!')
+            msg.exec_()
+    def upload(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Upload File","","Text files (*.txt)")
+        if fileName:
+            global data
+            self.uploadedFile = fileName
+            self.fileName.setText(os.path.basename(fileName))
+            self.namaFile = os.path.basename(fileName)
+            data = open(fileName,"rt")
+            fileBaru = open(self.namaFile,'w')
+            fileBaru.write(str(data.read()))
+        else:
+            print("No file selected")     
+
+    def result(self):
+        nilaiP = self.nilaip.text()
+        nilaiQ = self.nilaiq.text()
+        if nilaiP == '' or nilaiQ == '' or self.namaFile == '':
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error")
+            msg.setInformativeText('Pastikan seluruh field telah terisi!')
+            msg.exec_()  
+        else:
+            privKeyFile = open('PrivateKey.pri','r')
+            privKey = int(privKeyFile.read())
+            insertButtonSameFile(self.namaFile,int(nilaiP),int(nilaiQ),privKey)
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("Berhasil!")
+            msg.setInformativeText('Tanda Tangan Berhasil diinput!')
+            msg.exec_()  
+app = QApplication(sys.argv)
+widget = QtWidgets.QStackedWidget()
+widget.addWidget(Landing())  # Index jadi 0
+widget.addWidget(Satu())  # Index jadi 1
+widget.addWidget(VerifSatu())  # Index jadi 2
+widget.addWidget(InsertSatu())  # Index jadi 3
+widget.setCurrentIndex(0)
+widget.setFixedWidth(800)
+widget.setFixedHeight(320)
+widget.show()
+
+app.exec_()
