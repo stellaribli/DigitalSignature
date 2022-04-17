791e260ced2027efd9e69cead9fc2f0c30e10a74
dionne22 <dionne.aurelia@gmail.com>
2022-04-17 11:07:43 +0700

    add sign terpisah
    Add digital signature untuk file terpisah

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import os.path
 import sys
-from numpy import insert
+# from numpy import insert
 from PyQt5.QtWidgets import QDialog, QApplication, QMessageBox, QFileDialog
 from PyQt5 import QtWidgets
 from PyQt5.uic import loadUi
@@ -75,14 +75,39 @@ def encSignature(h,n,Key):
     result = (h**Key) % n
     return result
 
+#File Terpisah
+def insertSignaturePisah(filename,p, q, privKey): #export signature to a new file
+    # file = open(filename,'rt')
+
+    # file.close()
+    signature = encSignature(hashSHA(filename),p*q,privKey)
+    fileBaru = open('Sign.txt', 'w')
+    teksSign = '<ds>' + str(signature) + '</ds>'
+    
+    # file.write(teksawal)
+    fileBaru.write(teksSign)
+    fileBaru.close()
+    return 
+def verifyPisah(filename,filesignature,p,q,pubKey):
+    signature = readSignatureFile(filesignature)
+    if signature == 'Error':
+        return False
+    else:
+        signature = int(signature)
+        hAksen = encSignature(signature,p*q,pubKey)
+        hashedPass = int(hashSHA(filename))%(p*q)
+        if hAksen == hashedPass:
+            return True
+        else:
+            return False
+
 #File Tidak Terpisah
 def insertSignatureSameFile(filename,signature): #Returns New File with Signature
     file = open(filename,'rt')
     teksawal = file.read()
     fileBaru = open(filename, 'w')
-    fileBaru.write(teksawal)
     teksSign = '<ds>' + str(signature) + '</ds>'
-    fileBaru.write(teksSign)
+    fileBaru.write(teksawal + teksSign)
     file.close()
     fileBaru.close()
     return
@@ -103,6 +128,8 @@ def insertButtonSameFile(filename,p,q,privKey):
     signature = encSignature(hashSHA(filename),p*q,privKey)
     insertSignatureSameFile(filename,signature)
 
+
+
 #VerifyButton
 def verifyButtonSameFile(filename,p,q,pubKey):
     signature = readSignatureFile(filename)
@@ -117,13 +144,18 @@ def verifyButtonSameFile(filename,p,q,pubKey):
         else:
             return False
 
+
+
 class Landing(QDialog):
     def __init__(self):
         super(Landing, self).__init__()
         loadUi("landingpage.ui", self)
         self.disatukan.clicked.connect(self.goToSatukan)
+        self.terpisah.clicked.connect(self.goToPisah)
     def goToSatukan(self):
         widget.setCurrentIndex(1)
+    def goToPisah(self):
+        widget.setCurrentIndex(4)
 
 class Satu(QDialog):
     def __init__(self):
@@ -137,7 +169,161 @@ class Satu(QDialog):
     def goToVerif(self):
         widget.setCurrentIndex(2)    
     def goToInsert(self):
-        widget.setCurrentIndex(3)    
+        widget.setCurrentIndex(3)
+
+class Pisah(QDialog):
+    def __init__(self):
+        super(Pisah, self).__init__()
+        loadUi("pisah.ui", self)
+        self.backButton.clicked.connect(self.goBack)
+        self.verifikasi.clicked.connect(self.goToVerifPisah)
+        self.insert.clicked.connect(self.goToInsertPisah)
+    def goBack(self):
+        widget.setCurrentIndex(0)    
+    def goToVerifPisah(self):
+        widget.setCurrentIndex(6)    
+    def goToInsertPisah(self):
+        widget.setCurrentIndex(5)    
+
+class InsertPisah(QDialog):
+    def __init__(self):
+        super(InsertPisah, self).__init__()
+        loadUi("insertSigPisah.ui", self)
+        self.backButton.clicked.connect(self.goBack)
+        self.genButton.clicked.connect(self.genKey)
+        self.uploadButton.clicked.connect(self.upload)
+        self.uploadedFile = None 
+        self.namaFile = ''
+        self.insertSig.clicked.connect(self.result)
+    def goBack(self):
+        widget.setCurrentIndex(4)  
+    def genKey(self):
+        nilaiP = self.nilaip.text()
+        nilaiQ = self.nilaiq.text()
+        if nilaiP == '' or nilaiQ == '':
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error")
+            msg.setInformativeText('P dan Q belum diinput!')
+            msg.exec_()
+        else:
+            generateKey(int(nilaiP),int(nilaiQ))
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("Berhasil")
+            msg.setInformativeText('Kunci Publik dan Private Berhasil Dibuat!')
+            msg.exec_()
+    def upload(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Upload File","","Text files (*.txt)")
+        if fileName:
+            global data
+            self.uploadedFile = fileName
+            self.fileName.setText(os.path.basename(fileName))
+            self.namaFile = str(os.path.basename(fileName))
+            data = open(fileName,"rt")
+            text = data.read()
+            data.close()
+            fileBaru = open(self.namaFile,'w')
+            fileBaru.write(str(text))
+            fileBaru.close()
+  
+    def result(self):
+        nilaiP = self.nilaip.text()
+        nilaiQ = self.nilaiq.text()
+        if nilaiP == '' or nilaiQ == '' or self.namaFile == '':
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error")
+            msg.setInformativeText('Pastikan seluruh field telah terisi!')
+            msg.exec_()  
+        else:
+            privKeyFile = open('PrivateKey.pri','r')
+            privKey = int(privKeyFile.read())
+            insertSignaturePisah(self.namaFile,int(nilaiP),int(nilaiQ),privKey)
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("Berhasil!")
+            msg.setInformativeText('Tanda Tangan Berhasil!')
+            msg.exec_()
+
+class VerifPisah2(QDialog):
+    def __init__(self):
+        super(VerifPisah2, self).__init__()
+        loadUi("verifSigPisah2.ui", self)
+        self.backButton.clicked.connect(self.goBack)
+        self.uploadButton.clicked.connect(self.upload)
+        self.uploadPub.clicked.connect(self.uploadKey)
+        self.uploadSign.clicked.connect(self.uploadSignature)
+        self.uploadedFile = None 
+        self.uploadedPub = None 
+        self.uploadedSignature = None
+        self.namaFile = ''
+        self.verifikasi.clicked.connect(self.result)
+    def goBack(self):
+        widget.setCurrentIndex(4) 
+    def upload(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Upload File","","Text files (*.txt)")
+        if fileName:
+            global data
+            self.uploadedFile = fileName
+            self.fileName.setText(os.path.basename(fileName))
+            self.namaFile = os.path.basename(fileName)
+            file = open(fileName,"rt")
+            data = file.read()
+            file.close()
+            fileBaru = open(self.namaFile,'w')
+            fileBaru.write(str(data))
+            fileBaru.close()
+    def uploadSignature(self):
+        signFile, _ = QFileDialog.getOpenFileName(self, "Upload File","","Text files (*.txt)")
+        if signFile:
+            global dataSign
+            self.uploadedSignature = signFile
+            self.fileName3.setText(os.path.basename(signFile))
+            self.namaFileSign = os.path.basename(signFile)
+            file = open(signFile,"rt")
+            dataSign = file.read()
+            file.close()
+            fileBaru = open(self.namaFileSign,'w')
+            fileBaru.write(str(dataSign))
+            fileBaru.close()
+    def uploadKey(self):
+        pubFile, _ = QFileDialog.getOpenFileName(self, "Upload File","","Text files (*.pub *.txt)")
+        if pubFile:
+            global dataPub
+            self.uploadedPub = pubFile
+            self.fileName2.setText(os.path.basename(pubFile))
+            file = open(pubFile,"rt")
+            dataPub = file.read()
+            file.close()
+            fileBaru = open('PublicKey.pub','w')
+            fileBaru.write(str(dataPub))
+            fileBaru.close()    
+    def result(self):
+        nilaiP = self.nilaip.text()
+        nilaiQ = self.nilaiq.text()
+        if nilaiP == '' or nilaiQ == '' or self.namaFile == '':
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error")
+            msg.setInformativeText('Pastikan seluruh field telah terisi!')
+            msg.exec_()  
+        else:
+            pubKeyFile = open('PublicKey.pub','r')
+            pubKey = int(pubKeyFile.read())
+            hasilVerifikasi = verifyPisah(self.namaFile,self.namaFileSign,int(nilaiP),int(nilaiQ),pubKey)
+            if hasilVerifikasi:
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Information)
+                msg.setText("Berhasil!")
+                msg.setInformativeText('Tanda Tangan Terverifikasi')
+                msg.exec_()         
+            else:
+                msg = QMessageBox()
+                msg.setIcon(QMessageBox.Critical)
+                msg.setText("Gagal")
+                msg.setInformativeText('Tanda Tangan Tidak Berhasil Diverifikasi')
+                msg.exec_()    
 
 class VerifSatu(QDialog):
     def __init__(self):
@@ -239,8 +425,11 @@ class InsertSatu(QDialog):
             self.fileName.setText(os.path.basename(fileName))
             self.namaFile = str(os.path.basename(fileName))
             data = open(fileName,"rt")
+            text = data.read()
+            data.close()
             fileBaru = open(self.namaFile,'w')
-            fileBaru.write(str(data.read()))
+            fileBaru.write(str(text))
+            fileBaru.close()
   
     def result(self):
         nilaiP = self.nilaip.text()
@@ -254,6 +443,7 @@ class InsertSatu(QDialog):
         else:
             privKeyFile = open('PrivateKey.pri','r')
             privKey = int(privKeyFile.read())
+            # privKeyFile.close()
             insertButtonSameFile(self.namaFile,int(nilaiP),int(nilaiQ),privKey)
             msg = QMessageBox()
             msg.setIcon(QMessageBox.Information)
@@ -267,6 +457,10 @@ widget.addWidget(Landing())  # Index jadi 0
 widget.addWidget(Satu())  # Index jadi 1
 widget.addWidget(VerifSatu())  # Index jadi 2
 widget.addWidget(InsertSatu())  # Index jadi 3
+
+widget.addWidget(Pisah())  # Index jadi 4
+widget.addWidget(InsertPisah()) # Index jadi 5
+widget.addWidget(VerifPisah2()) # Index jadi 6
 widget.setCurrentIndex(0)
 widget.setFixedWidth(800)
 widget.setFixedHeight(320)
